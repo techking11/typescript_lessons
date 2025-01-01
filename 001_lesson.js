bd890454e7b539a3a4983b19c7ea3c920334e359
techking11 <nma1132000@gmail.com>
2025-01-01 11:23:24 +0700

    Test: Type annotations

---
+++ 001_lesson.js
@@ -0,0 +1,6 @@
+var age = 25;
+var isStudent = true;
+function greet(name) {
+    return "Hello ".concat(name, "!");
+}
+console.log(greet("Justin"));
---
+++ 001_lesson.ts
@@ -0,0 +1,8 @@
+let age: number = 25;
+let isStudent: boolean = true;
+
+function greet(name: string): string {
+  return `Hello ${name}!`;
+}
+
+console.log(greet("Justin"));
