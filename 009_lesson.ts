b2c81d412a876825517163582a6dc18c59d05b20
techking11 <nma1132000@gmail.com>
2025-01-02 23:24:22 +0700

    Test: add turple

---
+++ 009_lesson.ts
@@ -0,0 +1,11 @@
+// Turple
+
+let day: [number, string];
+day = [0, "Monday"];
+// day = ['zero', 'Sunday']; // invalid: 'zero' is not number
+
+console.log(day[0]); // Output: 0
+console.log(day[1]); // Output: Monday
+
+// day[2] = "Saturday"; // invalid: no element in index 2
+// day[3] = false; // invalid: boolean type not assignable
