b23106ca01d60d4576ade484ddbd452893c59cce
techking11 <nma1132000@gmail.com>
2025-01-03 08:24:41 +0700

    Test: add array

---
+++ 011_lesson.ts
@@ -0,0 +1,12 @@
+// Array
+let threePigsArr: (number | string | boolean)[] = [0, 23, 11, "monday", true];
+
+let genericStringArr: Array<string | number | boolean> = [
+  "first",
+  "second",
+  "third",
+  23,
+  567,
+  true,
+];
+// you can use the array as you like
