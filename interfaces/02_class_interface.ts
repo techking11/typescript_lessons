ff9c1d4ca6e695c991386c9cdf94b02f3d8e536b
techking11 <nma1132000@gmail.com>
2025-01-03 22:46:00 +0700

    Test: add class interface

---
+++ interfaces/02_class_interface.ts
@@ -0,0 +1,25 @@
+interface ISampleClassInterface {
+  sampleVariable: string;
+
+  sampleMethod(): void;
+
+  optionalVariable?: string;
+}
+
+class SampleClass implements ISampleClassInterface {
+  public sampleVariable: string;
+  private answerToLifeTheUniverseAndEverything: number;
+
+  constructor() {
+    this.sampleVariable = "string value";
+    this.answerToLifeTheUniverseAndEverything = 42;
+  }
+
+  public sampleMethod(): void {
+    // do nothing
+  }
+
+  private answer(q: any): number {
+    return this.answerToLifeTheUniverseAndEverything;
+  }
+}
