37d303383620199a0b8c44de7868eb81a5defbe7
techking11 <nma1132000@gmail.com>
2025-01-04 10:03:05 +0700

    Test: add strick null check, non null assertion, user defined type guard, mixin

---
+++ notes.ts
@@ -0,0 +1,29 @@
+/**
+ * Strict Null Check
+ * =================================================
+ * | by default all types in typescript allow null
+ * | don't forget to check null
+ * | run with cmd: --strictNullChecks
+ * =================================================
+ *
+ * Non Null Assertion
+ * =================================================
+ * | ! don't allow null or underfined
+ * | node.next!.data
+ * | ts compiller don't infer that automatically
+ * =================================================
+ *
+ * User Defined Type Guard
+ * =================================================
+ * | Type guard fns
+ * | fn(variableName: any): variableName is string
+ * |------------------------------------------------
+ * | animal instanceOf Dog
+ * | class Dog {} | animal: any
+ * =================================================
+ *
+ * Mixins
+ * | use by combining many classes if you need
+ *
+ *
+ */
